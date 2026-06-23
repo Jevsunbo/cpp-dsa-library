82dac9a7edba6a8a970ba1ba91d3f0b834098b75
jevsunbo <jevsunbo04@gmail.com>
2026-06-22 22:46:14 -0700

    Updated move zero file

--- algorithms/arrays/move_zeroes_283.cpp
+++ algorithms/arrays/move_zeroes_283.cpp
@@ -39,11 +39,17 @@ public:
 
         for (int fast = 0; fast < nums.size(); fast++) {
             if (nums[fast] != 0) {
+                int temp = nums[slow];
                 nums[slow] = nums[fast];
-                if (slow != fast) nums[fast] = 0;
+                nums[fast] = temp;
                 slow++;
             }
         }
+    }
+
+    void moveZerosv2(vector<int>& nums){
+        int slow = 0;
+        for(int =  )
     }
 };
 
---
+++ patterns/pattern_review/two_pointers_slow_fast.cpp
@@ -0,0 +1,6 @@
+// [ ] Two pointers slow/fast — what it is, when to use it
+// Two pointers slow/fast is a pattern 
+#include <iostream>
+using namespace std;
+
+
