4e18d2e7e2c6a0cc5e52aa24b4a85939cf1592ee
jevsunbo <jevsunbo04@gmail.com>
2026-07-17 23:13:59 -0700

    implemented assert function to test the linked list file

--- tests/linked_list_test.cpp
+++ tests/linked_list_test.cpp
@@ -1,31 +1,15 @@
 #include "dsa/linked_list.h"
 #include <iostream>
+#include <cassert>
+
+// test functions:
+void test_empty_list(){ 
+  dsa::LinkedList list;
+  assert(list.size()==0);
+  assert(list.has_cycle()==false);
+  std::cout << "test_empty_list PASSED" << std::endl;
+}
 
 int main(){
-// lets test to see if the list was properly allocated with the size() function
-    dsa::LinkedList list;
-    std::cout << "Here is the size of the list: " << list.size() << "." << std::endl;
-
-// now lets test out the other member functions of LinkedList
-    list.push_back(10);
-    list.push_front(21);
-    list.print();
-
-    std::cout << "Now here is the size of the list: " << list.size() << "." << std::endl;
-
-// now to test reverse
-    list.push_back(8);
-    list.push_back(9);
-    list.print();
-    std::cout << std::endl;
-    list.reverse();
-    list.print();
-
-    if(list.has_cycle()){
-      std::cout << "It has a cycle!!!" << std::endl;
-    }
-    else{
-      std::cout << "It does NOT have a cycle!" << std::endl;
-    }
-
+  test_empty_list();
 }
